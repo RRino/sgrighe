83673a56c07e839f196ef00fcaf2044f9db4fe3c
RRino <rino.ruggeri@gmail.com>
2023-08-12 21:41:34 +0200

    modifiche al menu top

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,21 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Summernote Text Editor CRUD and Image Upload in Laravel</title>
-  <!-- bootstrap -->
-  <link href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" rel="stylesheet">
-
-</head>
+@extends('layouts.app')
 <body>
 
 
-
+  @section('content')
   <div class="container p-4 ">
     <div class="text-center">
-      <h1 class="">Index</h1>
+      <h1 class="">Articoli</h1>
     </div>
     <a href="/create" class="btn btn-md btn-primary">Nuovo Articolo</a>
     <a href="/" class="btn btn-md btn-success">Home</a>
@@ -48,6 +38,5 @@
       </table>
 
     </div>
-  
+@endsection
 </body>
-</html>
